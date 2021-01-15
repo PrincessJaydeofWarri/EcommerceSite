966fe8002ecac0fb8a612e3b2252dc68ef8e0ef8
Jayde Pearson <etana@Astute.local>
2021-01-15 21:49:16 +0000

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,46 @@
+const products = [
+    
+    {
+        name:"Abramad",
+        imageUrl:"./images/Abramad.png",
+        description:"Made by BDK Parfums signiture smell a little but of you",
+        price: 85,
+        countInStock:3,
+    },
+    {
+        name:"Bouquet",
+        imageUrl:"./images/Bouquet.png",
+        description:"Based off the signiture fragrance by Lancome BDK adds sandal wood to lift the frangrance",
+        price: 70,
+        countInStock:7,
+    },
+    {
+        name:"Greatness",
+        imageUrl:"./images/Greatness.png",
+        description:"A rich floral woody fragrance, containing jasmine sambac and white amber",
+        price: 45,
+        countInStock:1,
+    },
+    {
+        name:"Jasmins",
+        imageUrl:"./images/Jasmins.png",
+        description:"Opening with a fruity burst of pomegranate, Calvin Klein Euphoria is distinct yet not overpowering. Its oriental-inspired ingredients create a lasting signature, with fruity and floral scents against a rich and creamy fragrance. ",
+        price: 40,
+        countInStock:25,
+    },
+    {
+        name:"NewYork",
+        imageUrl:"./images/NewYork.png",
+        description:"A modern and fruity scent, bringing together exotic flowers with fruits and a woody finish, for a well-balanced fragrance.",
+        price: 120,
+        countInStock:4,
+    },
+    {
+        name:"SideEffect",
+        imageUrl:"/images/sideEffect.png",
+        description:"Lingering sensual base notes of sweet toffee melt with Indonesian patchouli to leave a subtle, sensual memory on the skin. ",
+        price: 60,
+        countInStock:10,
+    },
+];
+module.exports = products;
