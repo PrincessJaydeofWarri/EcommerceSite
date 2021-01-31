6a0510006e0a77c58c012fed47a60bb33a4ade24
Jayde Pearson <etana@Astute.local>
2021-01-31 16:38:43 +0000

    adding redux actions, constants and reducers

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,24 @@
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+
+//Reducers
+import {cartReducer} from '../redux/reducers/cartReducers';
+import {getProductsReducer, getProductDetailsReducer} from './reducers/product.Reducers'
+
+const reducer = combineReducers({
+    cart: cartReducer,
+    getProducts: getProductsReducer,
+    getProductDetails: getProductDetailsReducer
+});
+
+const middleware = [thunk];
+
+const store = createStore (
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
+
