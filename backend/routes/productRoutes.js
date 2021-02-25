a6adcbb3256d19b14d7d333ae7cc971ff40ec982
Jayde Pearson <etana@Astute.local>
2021-02-25 16:17:01 +0000

    update routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,4 @@
-const exppress = require('express');
+const express = require('express');
 const router = express.Router();
 
 const {getProducts, getProductById} = require('../controller/productControllers');
